8e852930972b1bc54c73076b8ffee57cab784754
ValenGu1t0 <devalen543@gmail.com>
2025-02-17 19:07:19 -0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// Forma lenta, pero permite importar de a muchas cosas
+import { Button } from "@mui/material";
+
+// Forma rapida, pero solo permite seleccionar un Componente
+/* import Button from "@mui/material/Button" */
+
+function App() {
+
+  // useState para home y about
+  // useState para modales (3) -> component home 
+
+    return (
+        <div>
+
+          {/* Header */}
+
+          <h1>Hola Mundo!!</h1>
+          <Button variant="contained">Mi primer MUI</Button>
+          
+          {/*  */}
+
+          {/* Footer */}
+        </div>
+    );
+}
+
+export default App;
